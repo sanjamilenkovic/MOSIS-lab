356eb85ff2ca425c3ccb2a652d36968952ab7275
5ardjordjevic <petardjordjevi.1@gmail.com>
2021-04-05 23:00:28 +0200

    Almost finished

--- MyPlaces/app/src/main/java/rs/elfak/mosis/observers/myplaces/About.kt
+++ MyPlaces/app/src/main/java/rs/elfak/mosis/observers/myplaces/About.kt
@@ -2,10 +2,15 @@ package rs.elfak.mosis.observers.myplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        val ok = findViewById<Button>(R.id.about_ok)
+        ok.setOnClickListener {
+            finish()
+        }
     }
 }
--- MyPlaces/app/src/main/java/rs/elfak/mosis/observers/myplaces/MyPlacesList.kt
+++ MyPlaces/app/src/main/java/rs/elfak/mosis/observers/myplaces/MyPlacesList.kt
@@ -2,25 +2,38 @@ package rs.elfak.mosis.observers.myplaces
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-
+class MyPlacesList : AppCompatActivity() {
+    private val places: ArrayList<String> = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_my_places_list)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
 
+        places.add("Tvrdjava")
+        places.add("Cair")
+        places.add("Park Svetog Save")
+        places.add("Trg Kralja Milana")
+        val listView = findViewById<ListView>(R.id.my_place_list)
+        listView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, places)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_my_places_list, menu)
         return true
     }
 
@@ -31,12 +44,6 @@ class MainActivity : AppCompatActivity() {
 
         when (item.itemId) {
             R.id.show_map_item -> Toast.makeText(this, "Show Map!", Toast.LENGTH_SHORT).show()
-            R.id.my_places_list_item -> {
-                Toast.makeText(this, "My Places!", Toast.LENGTH_SHORT)
-                    .show()
-                var intent = Intent(this, MyPlacesList::class.java)
-                startActivity(intent)
-            }
             R.id.new_place_item -> Toast.makeText(this, "New Place!", Toast.LENGTH_SHORT).show()
             R.id.about_item -> {
                 Toast.makeText(this, "About!", Toast.LENGTH_SHORT).show()
@@ -48,5 +55,4 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
-
 }
